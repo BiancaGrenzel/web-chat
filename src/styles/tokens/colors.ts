b82d5719287f3cad8f9b7a78d31bd82d20b1113b
Bianca Grenzel Severo <biancagrenzel@gmail.com>
2022-09-12 12:45:33 -0300

    feat: style tokens

---
+++ src/styles/tokens/colors.ts
@@ -0,0 +1,26 @@
+export const blue = {
+  light: "#f4f7ff",
+  medium: "#A9BFF0",
+  neon: "#4A83FF",
+};
+
+export const black = {
+  light: "#B3B4B4",
+  medium: "#323435",
+  dark: "#1B1C1D",
+};
+
+export const white = {
+  light: "#FFFFFF",
+  medium: "#F9F9F9",
+  dark: "#E8E8E8",
+};
+
+export const orange = {
+  medium: "#EFAA86",
+};
+
+export const pink = {
+  medium: "#C38190",
+  neon: "#CE4161",
+};
