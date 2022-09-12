c6f9dc953ef4c7d043c9d4cc1553771e38c73497
Bianca Grenzel Severo <biancagrenzel@gmail.com>
2022-09-12 12:45:50 -0300

    feat: types

---
+++ src/types/style.ts
@@ -0,0 +1,17 @@
+export interface Style {
+  width?: string;
+  height?: string;
+  flexDirection?: string;
+  display?: string;
+  gap?: string;
+  alignItems?: string;
+  justifyContent?: string;
+  padding?: string;
+  margin?: string;
+  backgroundColor?: string;
+  overflow?: string;
+  overflowY?: string;
+  overflowX?: string;
+  tabletWidth?: string;
+  mobileWidth?:string;
+}
