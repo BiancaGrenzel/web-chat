e3a4cfeaa803dbb54b89ce9640bedeee85a069e2
Bianca Grenzel Severo <biancagrenzel@gmail.com>
2022-09-12 13:46:48 -0300

    feat: routes

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import "bootstrap/dist/css/bootstrap.min.css";
 import { DataContextProvider } from "./context";
+import Routes from "./routes";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -10,7 +9,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <DataContextProvider>
-      <App />
+      <Routes />
     </DataContextProvider>
   </React.StrictMode>
 );
