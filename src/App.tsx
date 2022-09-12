b1dc9289ad3c810db1ee6aed4cc9143ab9e6eb5c
Bianca Grenzel Severo <biancagrenzel@gmail.com>
2022-09-12 13:47:03 -0300

    feat: global style reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { ThemeProvider } from "styled-components";
 import * as Theme from "../src/styles/tokens/index";
 import Home from "./pages/Home";
+import GlobalStyle from "./styles/reset";
 
 function App() {
   return (
     <ThemeProvider theme={Theme}>
-      <Home/>
+      <GlobalStyle />
+      <Home />
     </ThemeProvider>
   );
 }
